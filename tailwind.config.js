0f44731fa29d2a6e27b9ec9d9c2d3ac90d825ba4
gabrielgreco11 <143386903+gabrielgreco11@users.noreply.github.com>
2023-10-23 09:18:33 +0200

    extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,28 +5,7 @@ module.exports = {
   './base.html','./index.html', './charts.html,', './sites.html', 'fokus_one_youtuber.html','./settings.html' ],
   theme: {
     extend: {
-      keyframes: {
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
-          },
-          "100%": {
-            width: "100%"
-          }  
-        },
-        blink: {
-          "50%": {
-            borderColor: "transparent"
-          },
-          "100%": {
-            borderColor: "white"
-          }  
-        }
-      },
-      animation: {
-        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
-      }
+      
     },
   },
   plugins: [],
